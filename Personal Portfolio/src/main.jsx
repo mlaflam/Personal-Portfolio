aadcd63b4ceb4935132e642945bd0fa0f67fdffd
mlaflam <mlaflam@users.noreply.github.com>
2024-05-20 13:47:04 -0400

    Setting up Project

--- Personal Portfolio/src/main.jsx
+++ Personal Portfolio/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import About from './routes/About.jsx';
+import Contact from './routes/Contact.jsx';
+import Projects from './routes/Projects.jsx';
+import Home from './routes/Home.jsx';
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  {path: "/About", element: <About/>},
+  {path: "/Contact", element: <Contact/>},
+  {path: "/Projects", element: <Projects/>}
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
